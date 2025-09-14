179f4625fd2105b4beabdf0ca8abfb2d622308d3
opisbin <hmeherab243@gmail.com>
2025-09-14 12:19:23 +0600

    Refactor code structure for improved readability and maintainability

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -82,8 +82,8 @@ const HeroSection = React.memo(() => {
   const renderHeroContent = (): React.ReactNode => (
     <div className={HERO_STYLES.CONTENT}>
       <div className="md:mb-4 mb-2">
-        <h2 className="text-4xl seq">Hello 👋🏻</h2>
-        <h1 className="text-3xl seq">I am Ayush Singh</h1>
+        <h2 className="text-4xl seq">Hello,你好👋🏻</h2>
+        <h1 className="text-3xl seq">I am Meherab Hossain</h1>
       </div>
       <p className="mb-4">
         <span className={HERO_STYLES.TYPED_SPAN} ref={typedSpanElement}></span>
@@ -98,13 +98,13 @@ const HeroSection = React.memo(() => {
             target: "_blank",
             rel: "noreferrer",
           }}
-          href="/Ayush_Resume.pdf"
+          href="/Meherab_Resume.pdf"
         ></Button>
         <Button
           classes="ml-3"
           type={ButtonTypes.PRIMARY}
           name="Let's Talk"
-          href={SOCIAL_LINKS.topmate}
+          href="https://www.linkedin.com/in/meherabh/"
           otherProps={{
             target: "_blank",
             rel: "noreferrer",
